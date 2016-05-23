fe340eb7200c304e55ef32cd5dd75247bd5b39d3
intrigeri <intrigeri@boum.org>
2016-05-23 21:09:25 +0000

    puppet::master::cleanup_reports: use a filename compatible with Debian's handling of cron.daily.
    At least on Debian Jessie, cron.daily is handled with run-parts, and "the names
    must consist entirely of ASCII upper- and lower-case letters, ASCII digits,
    ASCII underscores, and ASCII minus-hyphens".
    
    Closes: #9

--- manifests/master/cleanup_reports.pp
+++ manifests/master/cleanup_reports.pp
@@ -1,7 +1,13 @@
 class puppet::master::cleanup_reports {
   # clean up reports older than $puppetmaster_cleanup_reports days
-  file { '/etc/cron.daily/puppet_reports_cleanup.sh':
+  file { '/etc/cron.daily/puppet_reports_cleanup':
     content => "#!/bin/bash\nfind ${puppet::master::reports_dir} -maxdepth 2 -type f -ctime +${puppet::master::cleanup_reports} -exec rm {} \\;\n",
     owner => root, group => 0, mode => 0700;
+  }
+
+  # Clean up non-working cronjob set up by past versions of this class
+  # https://gitlab.com/shared-puppet-modules-group/puppet/issues/9
+  file { '/etc/cron.daily/puppet_reports_cleanup.sh':
+    ensure => absent,
   }
 }
